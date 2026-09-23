3345e684ae8e165f695700299eded389ce326918
Rafael Jordani Coelho <rafitapc08@gmail.com>
2026-09-22 21:16:54 -0300

    merge

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,1 +1,20 @@
-abstract class Home {}
+import 'package:flutter/material.dart';
+import 'package:sem_sufoco/core/theme/app_colors.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('')),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(height: 100, width: 100, color: AppColors.black),
+          ],
+        ),
+      ),
+    );
+  }
+}

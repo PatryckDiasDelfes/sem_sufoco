7f61ca8d1c2edbbd73d790c7143190fb639f9e66
Patryck Dias Delfes <delfesne@gmail.com>
2026-09-23 19:34:53 -0300

    refactor: clean up app_colors.dart and add LoginController widget

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -4,10 +4,8 @@ class AppColors {
   static const Color black = Colors.black;
   static const Color background = Color(0xFF01523B);
   static const Color primary = Color(0xFF009D71);
-   static const Color secondary = Color(0xFF5CCDA7);
-   static const Color tertiary = Color(0xFF98FFE0);
+  static const Color secondary = Color(0xFF5CCDA7);
+  static const Color tertiary = Color(0xFF98FFE0);
 
-static const Color cardGreen  = Color(0xFF009D71);
-static const Color background = Colors.black;
-static Color white = Colors.white
+  static const Color cardGreen = Color(0xFF009D71);
 }
